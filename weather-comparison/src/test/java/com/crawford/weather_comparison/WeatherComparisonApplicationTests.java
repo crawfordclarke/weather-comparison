87eff46d681a01898bbcec4b701f4c047529093e
crawfordclarke <crawfordclarke123@gmail.com>
2026-01-21 17:46:25 +0000

    Initial commit

---
+++ weather-comparison/src/test/java/com/crawford/weather_comparison/WeatherComparisonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crawford.weather_comparison;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherComparisonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
